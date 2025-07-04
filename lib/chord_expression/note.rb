d3e2478dd6e93d9c532610c2a285b5a62bd3325b
ohataken <11092425+ohataken@users.noreply.github.com>
2025-07-05 08:52:02 +0900

    feat: ChordExpression::Note#note_name

--- lib/chord_expression/note.rb
+++ lib/chord_expression/note.rb
@@ -1,4 +1,26 @@
 module ChordExpression
   class Note
+    def initialize(value)
+      @value = value
+    end
+
+    def note_name
+      case @value
+      when 0
+        'A'
+      when 2
+        'B'
+      when 3
+        'C'
+      when 5
+        'D'
+      when 7
+        'E'
+      when 8
+        'F'
+      when 10
+        'G'
+      end
+    end
   end
 end
