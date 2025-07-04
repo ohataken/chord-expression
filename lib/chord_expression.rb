c2ab04cabcf889b432432c8f45e984cab1b4d6e4
ohataken <11092425+ohataken@users.noreply.github.com>
2025-07-04 21:50:23 +0900

    feat: bundle gem chord_expression --test=rspec --no-linter

---
+++ lib/chord_expression.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "chord_expression/version"
+
+module ChordExpression
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/chord_expression/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ChordExpression
+  VERSION = "0.1.0"
+end
