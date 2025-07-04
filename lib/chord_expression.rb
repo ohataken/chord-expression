a412b7778525bf06e6eda9ed60164415038f4bea
ohataken <11092425+ohataken@users.noreply.github.com>
2025-07-04 21:59:00 +0900

    feat: add Note

--- lib/chord_expression.rb
+++ lib/chord_expression.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "chord_expression/version"
+require_relative "chord_expression/note"
 
 module ChordExpression
   class Error < StandardError; end
---
+++ lib/chord_expression/note.rb
@@ -0,0 +1,4 @@
+module ChordExpression
+  class Note
+  end
+end
