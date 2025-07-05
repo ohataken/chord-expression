244273cf54e7b2abd6d1c6a65f2d0fad6c259a75
ohataken <11092425+ohataken@users.noreply.github.com>
2025-07-05 18:19:42 +0900

    feat: ChordExpression::ChordParser#minor_major_seventh?

--- lib/chord_expression/chord_parser.rb
+++ lib/chord_expression/chord_parser.rb
@@ -1,4 +1,15 @@
 module ChordExpression
   class ChordParser
+    def initialize(chord_string)
+      @chord_string = chord_string
+    end
+
+    def parse_root_note
+      NoteParser.new(@chord_string).parse_note
+    end
+
+    def minor_major_seventh?
+      @chord_string.include?('mmaj7')
+    end
   end
 end
--- spec/chord_expression/chord_parser_spec.rb
+++ spec/chord_expression/chord_parser_spec.rb
@@ -1,4 +1,50 @@
 # frozen_string_literal: true
 
 RSpec.describe ChordExpression::ChordParser do
+  describe '#minor_major_seventh?' do
+    it 'returns false if the chord is A' do
+      chord_parser = ChordExpression::ChordParser.new('A')
+      expect(chord_parser.minor_major_seventh?).to be_falsey
+    end
+
+    it 'returns false if the chord is Bmin' do
+      chord_parser = ChordExpression::ChordParser.new('Bmin')
+      expect(chord_parser.minor_major_seventh?).to be_falsey
+    end
+
+    it 'returns false if the chord is Caug' do
+      chord_parser = ChordExpression::ChordParser.new('Caug')
+      expect(chord_parser.minor_major_seventh?).to be_falsey
+    end
+
+    it 'returns false if the chord is Ddim' do
+      chord_parser = ChordExpression::ChordParser.new('Ddim')
+      expect(chord_parser.minor_major_seventh?).to be_falsey
+    end
+
+    it 'returns false if the chord is Emaj7' do
+      chord_parser = ChordExpression::ChordParser.new('Emaj7')
+      expect(chord_parser.minor_major_seventh?).to be_falsey
+    end
+
+    it 'returns false if the chord is Fmin7' do
+      chord_parser = ChordExpression::ChordParser.new('Fmin7')
+      expect(chord_parser.minor_major_seventh?).to be_falsey
+    end
+
+    it 'returns false if the chord is G7' do
+      chord_parser = ChordExpression::ChordParser.new('G7')
+      expect(chord_parser.minor_major_seventh?).to be_falsey
+    end
+
+    it 'returns false if the chord is Adim7' do
+      chord_parser = ChordExpression::ChordParser.new('Adim7')
+      expect(chord_parser.minor_major_seventh?).to be_falsey
+    end
+
+    it 'returns true if the chord is Bmmaj7' do
+      chord_parser = ChordExpression::ChordParser.new('Bmmaj7')
+      expect(chord_parser.minor_major_seventh?).to be_truthy
+    end
+  end
 end
