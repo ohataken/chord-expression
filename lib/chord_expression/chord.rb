7d3d45e4ce230420b77c1385070738f3f722ccf0
ohataken <11092425+ohataken@users.noreply.github.com>
2025-07-07 02:53:30 +0900

    refactor: ChordExpression#major_third?

--- lib/chord_expression/chord.rb
+++ lib/chord_expression/chord.rb
@@ -31,13 +31,10 @@ module ChordExpression
     end
 
     def major_third?
-      return if @chord_parser.minor_major_seventh?
-      return if @chord_parser.minor_seventh?
-      return if @chord_parser.minor_triad?
-      return if @chord_parser.augmented_triad?
-      return if @chord_parser.diminished_triad?
-
-      true
+      return true if @chord_parser.major_seventh?
+      return true if @chord_parser.dominant_seventh?
+      return true if @chord_parser.major_triad?
+      return true if @chord_parser.augmented_triad?
     end
 
     def third
