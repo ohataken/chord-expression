db8971b59f4f9416ee47a3bf019e3e228c4d51ec
ohataken <11092425+ohataken@users.noreply.github.com>
2025-07-12 07:10:26 +0900

    feat: Scale supports minor scales

--- lib/chord_expression/scale.rb
+++ lib/chord_expression/scale.rb
@@ -11,92 +11,111 @@ module ChordExpression
     end
 
     def i_root_note
-      if major?
-        @root_note
-      end
+      @root_note
     end
 
     def ii_root_note
-      if major?
-        Note.new((@root_note.value + 2) % 12)
-      end
+      Note.new((@root_note.value + 2) % 12)
     end
 
     def iii_root_note
-      if major?
+      if @scale_parser.major?
         Note.new((@root_note.value + 4) % 12)
+      elsif @scale_parser.minor?
+        Note.new((@root_note.value + 3) % 12)
       end
     end
 
     def iv_root_note
-      if major?
-        Note.new((@root_note.value + 5) % 12)
-      end
+      Note.new((@root_note.value + 5) % 12)
     end
 
     def v_root_note
-      if major?
-        Note.new((@root_note.value + 7) % 12)
-      end
+      Note.new((@root_note.value + 7) % 12)
     end
 
     def vi_root_note
-      if major?
+      if @scale_parser.major?
         Note.new((@root_note.value + 9) % 12)
+      elsif @scale_parser.minor?
+        Note.new((@root_note.value + 8) % 12)
       end
     end
 
     def vii_root_note
-      if major?
+      if @scale_parser.major?
         Note.new((@root_note.value + 11) % 12)
+      elsif @scale_parser.minor?
+        Note.new((@root_note.value + 10) % 12)
       end
     end
 
     def i_seventh_chord
-      if major?
+      if @scale_parser.major?
         chord_parser = ChordParser.new("#{i_root_note.note_name_with_sharp}maj7")
+        chord_parser.parse
+      elsif @scale_parser.minor?
+        chord_parser = ChordParser.new("#{i_root_note.note_name_with_flat}min7")
         chord_parser.parse
       end
     end
 
     def ii_seventh_chord
-      if major?
+      if @scale_parser.major?
         chord_parser = ChordParser.new("#{ii_root_note.note_name_with_sharp}min7")
+        chord_parser.parse
+      elsif @scale_parser.minor?
+        chord_parser = ChordParser.new("#{ii_root_note.note_name_with_flat}min7flat5")
         chord_parser.parse
       end
     end
 
     def iii_seventh_chord
-      if major?
+      if @scale_parser.major?
         chord_parser = ChordParser.new("#{iii_root_note.note_name_with_sharp}min7")
+        chord_parser.parse
+      elsif @scale_parser.minor?
+        chord_parser = ChordParser.new("#{iii_root_note.note_name_with_flat}maj7")
         chord_parser.parse
       end
     end
 
     def iv_seventh_chord
-      if major?
+      if @scale_parser.major?
         chord_parser = ChordParser.new("#{iv_root_note.note_name_with_sharp}maj7")
+        chord_parser.parse
+      elsif @scale_parser.minor?
+        chord_parser = ChordParser.new("#{iv_root_note.note_name_with_flat}min7")
         chord_parser.parse
       end
     end
 
     def v_seventh_chord
-      if major?
+      if @scale_parser.major?
         chord_parser = ChordParser.new("#{v_root_note.note_name_with_sharp}7")
+        chord_parser.parse
+      elsif @scale_parser.minor?
+        chord_parser = ChordParser.new("#{v_root_note.note_name_with_flat}min7")
         chord_parser.parse
       end
     end
 
     def vi_seventh_chord
-      if major?
+      if @scale_parser.major?
         chord_parser = ChordParser.new("#{vi_root_note.note_name_with_sharp}min7")
+        chord_parser.parse
+      elsif @scale_parser.minor?
+        chord_parser = ChordParser.new("#{vi_root_note.note_name_with_flat}maj7")
         chord_parser.parse
       end
     end
 
     def vii_seventh_chord
-      if major?
+      if @scale_parser.major?
         chord_parser = ChordParser.new("#{vii_root_note.note_name_with_sharp}min7flat5")
+        chord_parser.parse
+      elsif @scale_parser.minor?
+        chord_parser = ChordParser.new("#{vii_root_note.note_name_with_flat}7")
         chord_parser.parse
       end
     end
