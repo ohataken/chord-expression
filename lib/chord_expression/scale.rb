b63e51d89f407c88b296dfdc7923341f3e9580be
ohataken <11092425+ohataken@users.noreply.github.com>
2025-07-11 23:13:34 +0900

    feat: ChordExpression::Scale#i_root_note

--- lib/chord_expression/scale.rb
+++ lib/chord_expression/scale.rb
@@ -1,4 +1,18 @@
 module ChordExpression
   class Scale
+    def initialize(root_note)
+      @root_note = root_note
+    end
+
+    # @todo
+    def major?
+      true
+    end
+
+    def i_root_note
+      if major?
+        @root_note
+      end
+    end
   end
 end
--- spec/chord_expression/scale_spec.rb
+++ spec/chord_expression/scale_spec.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe ChordExpression::Scale do
+  describe '#i_root_note' do
+    it 'returns the i root note' do
+      note = ChordExpression::Note.new(0)
+      scale = ChordExpression::Scale.new(note)
+      expect(scale.i_root_note.value).to eq(note.value)
+    end
+  end
 end
