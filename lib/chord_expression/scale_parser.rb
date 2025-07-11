05134eb4237d57fb1ff0309c5e40724778d8980e
ohataken <11092425+ohataken@users.noreply.github.com>
2025-07-12 07:04:56 +0900

    feat: ChordExpression::ScaleParser#root_note_parser

--- lib/chord_expression/scale_parser.rb
+++ lib/chord_expression/scale_parser.rb
@@ -1,4 +1,11 @@
 module ChordExpression
   class ScaleParser
+    def initialize(scale_string)
+      @scale_string = scale_string
+    end
+
+    def root_note_parser
+      NoteParser.new(@scale_string)
+    end
   end
 end
--- spec/chord_expression/scale_parser_spec.rb
+++ spec/chord_expression/scale_parser_spec.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 RSpec.describe ChordExpression::ScaleParser do
+  describe '#root_note_parser' do
+    it 'returns a NoteParser for A' do
+      parser = ChordExpression::ScaleParser.new('A')
+      note_parser = parser.root_note_parser
+      expect(note_parser).to be_a(ChordExpression::NoteParser)
+    end
+
+    it 'returns a NoteParser for Fsharp' do
+      parser = ChordExpression::ScaleParser.new('Fsharp')
+      note_parser = parser.root_note_parser
+      expect(note_parser).to be_a(ChordExpression::NoteParser)
+    end
+  end
 end
