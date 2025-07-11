d53662df4f446dd568441f93a040184b31f0974c
ohataken <11092425+ohataken@users.noreply.github.com>
2025-07-12 06:32:45 +0900

    feat: ChordExpression::ScaleParser

--- lib/chord_expression.rb
+++ lib/chord_expression.rb
@@ -6,6 +6,7 @@ require_relative "chord_expression/note_parser"
 require_relative "chord_expression/chord"
 require_relative "chord_expression/chord_parser"
 require_relative "chord_expression/scale"
+require_relative "chord_expression/scale_parser"
 
 module ChordExpression
   class Error < StandardError; end
---
+++ lib/chord_expression/scale_parser.rb
@@ -0,0 +1,4 @@
+module ChordExpression
+  class ScaleParser
+  end
+end
