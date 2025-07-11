1656fb837d6540470c963785efe3f7faffe644e4
ohataken <11092425+ohataken@users.noreply.github.com>
2025-07-11 23:13:53 +0900

    feat: ChordExpression::Scale#iii_root_note

--- lib/chord_expression/scale.rb
+++ lib/chord_expression/scale.rb
@@ -20,5 +20,11 @@ module ChordExpression
         Note.new((@root_note.value + 2) % 12)
       end
     end
+
+    def iii_root_note
+      if major?
+        Note.new((@root_note.value + 4) % 12)
+      end
+    end
   end
 end
--- spec/chord_expression/scale_spec.rb
+++ spec/chord_expression/scale_spec.rb
@@ -16,4 +16,12 @@ RSpec.describe ChordExpression::Scale do
       expect(scale.ii_root_note.value).to eq(note.value + 2)
     end
   end
+
+  describe '#iii_root_note' do
+    it 'returns the iii root note' do
+      note = ChordExpression::Note.new(0)
+      scale = ChordExpression::Scale.new(note)
+      expect(scale.iii_root_note.value).to eq(note.value + 4)
+    end
+  end
 end
